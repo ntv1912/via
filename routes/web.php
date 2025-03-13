841d7240e61e63e9f2e9a3d1be04dc02c3351cc5
ntvu <ntvu2kk3@gmail.com>
2025-03-13 16:47:48 +0700

    B2

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/user/{id}', function ($id) {
+    $users = [
+		[
+			"id" => 1,
+            "name" => "John Doe",
+            "gender"=> "nam",
+        ],
+        [
+			"id" => 2,
+            "name" => "Nguyễn Văn BB",
+            "gender"=> "nữ",
+        ],
+        [
+			"id" => 3,
+            "name" => "Trần Văn A",
+            "gender"=> "nam",
+        ],
+    ];
+    $user= collect($users)->map(function ($user) {
+        return (object) $user;
+    })->firstWhere('id', $id);
+    if(!$user){
+        abort(404);
+    }
+    return view('user',compact('user'));
+});
