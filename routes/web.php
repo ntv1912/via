b92857b8aa05951f9f7a2ff342accc56ae97a53d
ntvu <ntvu2kk3@gmail.com>
2025-03-13 17:16:46 +0700

    B3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::prefix('contact')->group(function () {
+    Route::get('/', function () {
+        return view('contact');
+    })->name('contact-form');
+    Route::post('/', function (Request $request) {
+        $namer = $request->input('name');
+        $email= $request->input('email');
+        return redirect('contact')->withInput()
+        ->with('success','Thành công')
+        ->with('name',$namer)
+        ->with('email',$email);
+    })->name('contact-submit');
+
 });
